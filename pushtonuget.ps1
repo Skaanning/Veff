6c8b4db5516dee320828f3159b8f542b8f1cca72
Michael Skaanning <mic.skaanning@gmail.com>
2025-12-14 14:22:25 +0100

    fix dateFlag UI bug and update pkg version

---
+++ pushtonuget.ps1
@@ -0,0 +1,4 @@
+dotnet nuget push .\src\Veff\bin\Release\Veff.3.0.4.nupkg --api-key  --source https://api.nuget.org/v3/index.json
+dotnet nuget push .\src\Veff.Snapshot\bin\Release\Veff.Snapshot.3.0.4.nupkg --api-key --source https://api.nuget.org/v3/index.json
+dotnet nuget push .\src\Veff.Sqlite\bin\Release\Veff.Sqlite.3.0.4.nupkg --api-key  --source https://api.nuget.org/v3/index.json
+dotnet nuget push .\src\Veff.SqlServer\bin\Release\Veff.SqlServer.3.0.4.nupkg --api-key  --source https://api.nuget.org/v3/index.json
